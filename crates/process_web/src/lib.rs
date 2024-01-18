bfb98233a996f3822fa810942b9007280c74242c
LiGuangNian <792841450@qq.com>
2024-01-18 16:28:31 +0800

    feat: update

---
+++ crates/process_web/src/lib.rs
@@ -0,0 +1,9 @@
+/// fn main() {
+///     if let Some(err) = api::start().err() {
+///         error!("Error: {}", anyhow!(err));
+///     }
+/// }
+
+pub mod api;
+pub mod entity;
+pub mod service;
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-use tracing::Level;
+use anyhow::anyhow;
+use tracing::error;
+
+
 pub mod scheduler;
 
-fn main() {
-    tracing_subscriber::fmt()
-        // all spans/events with a level higher than TRACE (e.g, info, warn, etc.)
-        // will be written to stdout.
-        .with_max_level(Level::TRACE)
-        // sets this to be the default, global subscriber for this application.
-        .init();
 
-    tracing::error!("SOMETHING IS SERIOUSLY WRONG!!!");
-    tracing::warn!("important informational messages; might indicate an error");
-    tracing::info!("general informational messages relevant to users");
-    tracing::debug!("diagnostics used for internal debugging of a library or application");
-    tracing::trace!("very verbose diagnostic events");
+fn main() {
+    if let Some(err) =  process_web::api::start().err() {
+        error!("Error: {}", anyhow!(err));
+    }
 }
