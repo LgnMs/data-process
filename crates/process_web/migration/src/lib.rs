7089a20634200c91b7575b3cbf36f8412c8a5f27
LiGuangNian <792841450@qq.com>
2024-03-04 16:00:19 +0800

    feat: 调整表创建顺序

--- crates/process_web/migration/src/lib.rs
+++ crates/process_web/migration/src/lib.rs
@@ -14,13 +14,13 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20240118_000001_create_collect_log_table::Migration),
-            Box::new(m20240118_000001_create_sync_log_table::Migration),
             Box::new(m20240119_000001_create_collect_config_table::Migration),
             Box::new(m20240119_023953_create_sync_config_table::Migration),
             Box::new(m20240226_015923_create_data_source_list::Migration),
             Box::new(m20240227_022320_create_data_sharing_config_table::Migration),
             Box::new(m20240304_063328_create_sharing_request_log::Migration),
+            Box::new(m20240118_000001_create_collect_log_table::Migration),
+            Box::new(m20240118_000001_create_sync_log_table::Migration),
         ]
     }
 }
