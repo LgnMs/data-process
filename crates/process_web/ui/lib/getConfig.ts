27bd012ac02650abd51ca58e241b3e4fe77a0ff0
LiGuangNian <792841450@qq.com>
2024-03-12 10:52:50 +0800

    feat: update

--- crates/process_web/ui/lib/getConfig.ts
+++ crates/process_web/ui/lib/getConfig.ts
@@ -1,5 +1,11 @@
+const { readFileSync } = require('fs');
+const path = require('path');
 
 // 在server环境中读取配置
 export async function getConfig() {
-  return await fetch(`http://localhost:${process.env.PORT}/config.json`, {cache: 'no-store'}).then(res => res.json());
+  const filePath = path.resolve('public/config.json');
+  const rawConfig = readFileSync(filePath);
+  const config = JSON.parse(rawConfig.toString());
+
+  return Promise.resolve(config)
 }
